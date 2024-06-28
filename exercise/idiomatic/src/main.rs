dba49e6bbfc8c3b2ab40a66098b44e844ba55119
beerhond <berend.sjaak@gmail.com>
2024-06-28 13:16:07 +0200

    Exercise done.

--- exercise/idiomatic/src/main.rs
+++ exercise/idiomatic/src/main.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 // 1. This code looks terrible. Let's start cleaning this up by running `cargo fmt`. If you
 // configured your editor or IDE to run `cargo fmt` automatically upon save, you can just save!
 
@@ -8,16 +10,24 @@
 
 // Challenge: Clippy doesn't find *everything*. What else would you change to make this code better?
 
-const pi:f32=3.14159265358979323846;
-fn count_to_5()->i32{let mut foo =0;loop{if foo>pi as i32{if foo > 5{break;}}foo=foo+1;}return 5;}
+fn count_to_5() -> i32 {
+    let mut dogfood = 0;
+    loop {
+        if dogfood > PI as i32 && dogfood > 5 {
+            break;
+        }
+        dogfood += 1;
+    }
+    5
+}
 fn main() {
-println!("I can count to {}", count_to_5());
+    println!("I can count to {}", count_to_5());
 }
 #[cfg(test)]
 mod test {
-use super::*;
-#[test]
-fn test_counting() {
-assert_eq!(count_to_5() == 5, true);
-}
+    use super::*;
+    #[test]
+    fn test_counting() {
+        assert_eq!(count_to_5(), 5);
+    }
 }
